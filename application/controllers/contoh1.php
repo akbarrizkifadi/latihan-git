da3f13bf7f4d29286fb3976fd1ab61c9911924fd
akbarrizkifadi <m.akbarrizkifadillah@gmail.com>
2023-09-25 15:42:27 +0700

    tambah ci

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Muhammad Akbar Rizki Fadillah
+        Saya tinggal di Jalan Flaboyan Srengseng Jakarta Barat
+        Olaharga yang saya sukai adalah Badminton";
+
+    }
+}
